6665ed642bc9848bc3ff5d48d5d83fa093b84ce1
GulfamKIIT <24156013@kiit.ac.in>
2026-01-09 22:37:43 +0530

    init commit

---
+++ evenodd/src/CompareStudent.java
@@ -0,0 +1,31 @@
+class Student {
+    int rollNo;
+    String name;
+
+    Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Student s = (Student) obj;
+        return rollNo == s.rollNo && name.equals(s.name);
+    }
+}
+public class CompareStudent {
+    public static void main(String[] args) {
+
+        Student s1 = new Student(101, "Gulfam");
+        Student s2 = new Student(101, "Gulfam");
+
+        if (s1.equals(s2)) {
+            System.out.println("Both students are equal");
+        } else {
+            System.out.println("Both students are not equal");
+        }
+    }
+}
