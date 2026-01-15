02ec03c21d6a5d38c6f3a41624af0e8d3d3598d4
GulfamKIIT <24156013@kiit.ac.in>
2026-01-15 17:17:07 +0530

    init commit

---
+++ evenodd/src/DepartmentHead.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+interface DeptEmployee {
+    void getDetails();
+}
+
+interface DeptManager extends DeptEmployee {
+    void getDeptDetails();
+}
+
+class DepartmentHead implements DeptManager {
+
+    int empId;
+    String empName;
+    int deptId;
+    String deptName;
+
+    Scanner sc = new Scanner(System.in);
+
+    public void getDetails() {
+        System.out.print("Enter employee id - ");
+        empId = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter employee name - ");
+        empName = sc.nextLine();
+    }
+
+    public void getDeptDetails() {
+        System.out.print("Enter department id - ");
+        deptId = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter department name - ");
+        deptName = sc.nextLine();
+    }
+
+    void display() {
+        System.out.println("Employee id - " + empId);
+        System.out.println("Employee name - " + empName);
+        System.out.println("Department id - " + deptId);
+        System.out.println("Department name - " + deptName);
+    }
+
+    public static void main(String[] args) {
+        DepartmentHead dh = new DepartmentHead();
+        dh.getDetails();
+        dh.getDeptDetails();
+        dh.display();
+    }
+}
---
+++ evenodd/src/WashingMachine.java
@@ -0,0 +1,31 @@
+// Defining the interface
+interface Motor {
+    int capacity = 5;
+
+    void run();
+    void consume();
+}
+
+
+class WashingMachine implements Motor {
+
+    public void run() {
+        System.out.println("Motor is running");
+    }
+
+    public void consume() {
+        System.out.println("Motor is consuming power");
+    }
+
+    public static void main(String[] args) {
+
+        WashingMachine wm = new WashingMachine();
+
+        wm.run();
+        wm.consume();
+
+
+        System.out.println("Capacity of the motor is " + capacity);
+    }
+}
+
